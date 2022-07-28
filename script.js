5a7b571f96072e7ae5b6a8230a2e587d97c3e12b
joan2305 <joanandreas875@gmail.com>
2022-07-29 02:55:41 +0700

    move several codes to index.html

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const items = document.querySelectorAll(".navbar-nav .nav-item .nav-link");
-items.forEach((item) => {
-  item.addEventListener("click", () => {
-    document.querySelector(".nav-link.active").classList.remove("active");
-    item.classList.add("active");
-  });
-});
+
